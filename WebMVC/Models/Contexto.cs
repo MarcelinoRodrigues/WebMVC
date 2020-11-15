1e845d77a353ce265c6d343bedba33d83d38b8fb
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2020-11-15 09:57:14 -0300

    add conection and using entity framework

---
+++ WebMVC/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
